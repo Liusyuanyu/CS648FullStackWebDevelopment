82cad61d8c167280d49cead14e4a51235c7ed17e
HsuanYu <syuanyu@hotmail.com.tw>
2020-02-01 00:33:23 -0800

    Assignment 1: Add correct url into the button.

--- assignment1/src/App.jsx
+++ assignment1/src/App.jsx
@@ -6,7 +6,7 @@ const element = (
         <div id="name" title="My Name">{name}</div>
         <img id="headshot" alt="headshot" src={headshot} accessKey="img"/>
         <div id="introduction" title="My Introduction">I am currently a graduate student in Computer Science at San Diego state University. I have token Machine Learning, Data mining, and Big data classes, etc. My skills include python, C#, and Java. Moreover, I was a software engineer for three years, and a data scientist intern for two months.</div>
-        <a href='https://www.google.com/'><button id="github">VIEW MY GITHUB REPO</button></a>
+        <a href='https://github.com/Liusyuanyu/CS648FullStackWebDevelopment.git'><button id="github">VIEW MY GITHUB REPO</button></a>
     </div>
     
 );
